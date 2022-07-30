e4faee4723451c626becc634854c2808515da0ac
mSafa <safaktrcgl@gmail.com>
2022-07-30 19:04:30 +0300

    Adjustments on file directories

--- TheCherno/HelloWorld/HelloWorld/Math.cpp
+++
@@ -1,9 +0,0 @@
-#define INTEGER int
-
-//void Log(const char* message);	//Declaration
-
-//INTEGER Multiply(int a, int b)
-//{
-//	Log("Multiplication");
-//	return a * b;
-//}
--- TheCherno/HelloWorld/HelloWorld/Static.cpp
+++
@@ -1,1 +0,0 @@
-static int s_Var = 5;
